9bd54e66dd931cd7181ffb27328af4182970e8ce
sayaten <ilyfstory@gmail.com>
2015-06-05 04:05:08 +0900

    test

--- FaceLinkerServer/src/Server/SocketServer.java
+++ FaceLinkerServer/src/Server/SocketServer.java
@@ -17,8 +17,8 @@ public class SocketServer {
 	public static void main(String[] args) throws IOException{
 		while(true)
 		{
-			serverSocket = new ServerSocket(port);
 			serverSocket.setReuseAddress(true);
+			serverSocket = new ServerSocket(port);
 			
 			while(true) {
 				clientSocket = serverSocket.accept();
