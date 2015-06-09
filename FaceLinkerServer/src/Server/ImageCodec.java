18b8872be8b2ec3ac25f510d66c79dea9d0a8389
sayaten <ilyfstory@gmail.com>
2015-06-09 10:24:24 +0900

    add save thumbnail image code

--- FaceLinkerServer/src/Server/ImageCodec.java
+++ FaceLinkerServer/src/Server/ImageCodec.java
@@ -1,5 +1,9 @@
 package Server;
 
+import java.awt.Color;
+import java.awt.Image;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
@@ -7,8 +11,13 @@ import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 
+import javax.imageio.ImageIO;
+
 public class ImageCodec {
 	public static final String IMG_ROOT_DIR = "/home/saya/Project/FLImages/";
+	public static final String RESIZE_WITH_WIDTH = "WIDTH";
+	public static final String RESIZE_WITH_HEIGHT = "HEIGHT";
+
 	public static void saveImage(byte[] data, String dir, String name){
 		File ofile = new File(IMG_ROOT_DIR + dir + "/" + name);
 		FileOutputStream fos;
@@ -37,6 +46,106 @@ public class ImageCodec {
 		}catch(IOException e){
 			e.printStackTrace();
 		}
+		return bos.toByteArray();
+	}
+
+	public static void saveThumbnailImage (byte[] data, String dir, String name, float ratio){
+		byte[] thumbnail = resizeImage(data, ratio);
+		saveImage(thumbnail, dir, name);
+	}
+
+	public static void saveThumbnailImage (byte[] data, String dir, String name, int width, int height){
+		byte[] thumbnail = resizeImage(data, width, height);
+		saveImage(thumbnail, dir, name);
+	}
+	
+	public static void saveThumbnailImage (byte[] data, String dir, String name, int length, String type){
+		byte[] thumbnail = resizeImage(data, length, type);
+		saveImage(thumbnail, dir, name);
+	}
+	
+	public static byte[] resizeImage(byte[] byteImage, int width, int height){
+		ByteArrayInputStream bis = new ByteArrayInputStream(byteImage);
+		BufferedImage image;
+		Image scaledImage;
+		BufferedImage imageBuf;
+		ByteArrayOutputStream bos = new ByteArrayOutputStream();
+		try{
+			image = ImageIO.read(bis);
+			if(height == 0){
+				height = (width * image.getHeight()) / image.getWidth();
+			}
+			if(width == 0){
+				width = (height * image.getWidth()) / image.getHeight();
+			}
+			scaledImage = image.getScaledInstance(width, height, Image.SCALE_SMOOTH);
+			imageBuf = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+			imageBuf.getGraphics().drawImage(scaledImage, 0, 0, new Color(0,0,0), null);
+			
+			ImageIO.write(imageBuf, "jpg", bos);
+		} catch(IOException e){
+			e.printStackTrace();
+		}
+		
+		return bos.toByteArray();
+	}
+	
+	public static byte[] resizeImage(byte[] byteImage, int length, String type){
+		ByteArrayInputStream bis = new ByteArrayInputStream(byteImage);
+		BufferedImage image;
+		Image scaledImage;
+		BufferedImage imageBuf;
+		ByteArrayOutputStream bos = new ByteArrayOutputStream();
+		try{
+			image = ImageIO.read(bis);
+			
+			switch(type){
+			case RESIZE_WITH_WIDTH:
+				scaledImage = image.getScaledInstance(length, image.getHeight() * length / image.getWidth(), Image.SCALE_SMOOTH);
+				imageBuf = new BufferedImage(length, image.getHeight() * length / image.getWidth() , BufferedImage.TYPE_INT_RGB);
+				imageBuf.getGraphics().drawImage(scaledImage, 0, 0, new Color(0,0,0), null);
+				ImageIO.write(imageBuf, "jpg", bos);
+				break;
+			case RESIZE_WITH_HEIGHT:
+				scaledImage = image.getScaledInstance(image.getWidth() * length / image.getHeight() , length, Image.SCALE_SMOOTH);
+				imageBuf = new BufferedImage(image.getWidth() * length / image.getHeight(), length, BufferedImage.TYPE_INT_RGB);
+				imageBuf.getGraphics().drawImage(scaledImage, 0, 0, new Color(0,0,0), null);
+				ImageIO.write(imageBuf, "jpg", bos);
+				break;
+			default:
+				System.out.println("Unvalued standard!!");
+				return byteImage;
+			}
+		} catch(IOException e){
+			e.printStackTrace();
+		}
+		
+		return bos.toByteArray();
+	}
+	
+	public static byte[] resizeImage(byte[] byteImage, float ratio){
+		ByteArrayInputStream bis = new ByteArrayInputStream(byteImage);
+		BufferedImage image;
+		Image scaledImage;
+		BufferedImage imageBuf;
+		ByteArrayOutputStream bos = new ByteArrayOutputStream();
+		try{
+			image = ImageIO.read(bis);
+			
+			if (ratio <= 0.0f){
+				return byteImage;
+			}
+			
+			scaledImage = image.getScaledInstance((int)(image.getWidth() * ratio), (int)(image.getHeight() * ratio), Image.SCALE_SMOOTH);
+			imageBuf = new BufferedImage((int)(image.getWidth() * ratio), (int)(image.getHeight() * ratio), BufferedImage.TYPE_INT_RGB);
+			imageBuf.getGraphics().drawImage(scaledImage, 0, 0, new Color(0,0,0), null);
+			
+			
+			ImageIO.write(imageBuf, "jpg", bos);
+		} catch(IOException e){
+			e.printStackTrace();
+		}
+		
 		return bos.toByteArray();
 	}
 }
